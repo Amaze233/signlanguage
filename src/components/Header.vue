5d1bbde64b1563e91ad478d1fa79b82b8fa570df
XSY <13707064031@163.com>
2021-12-23 16:24:10 +0800

    手语识别前端vue

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+// @ClassName: Header
+// @Author: XIAO SIYU
+// @Date: 2021/12/23 16:22
+// @Version: 1.0
+// @Description: TODO
+
+
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
